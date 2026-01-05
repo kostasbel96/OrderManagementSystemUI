095f6eb5dff1ecd5beb3b131df2d6f9adc48e9bc
Kostas Veloutsos <kwstasvel96@gmail.com>
2026-01-05 21:50:04 +0200

    feat: create navbar

--- src/components/NavbarComponent.tsx
+++ src/components/NavbarComponent.tsx
@@ -1,7 +1,20 @@
+import logo from "../assets/logo_ed.png"
+
 const NavbarComponent = () => {
     return (
         <>
-
+            <nav className="flex justify-center flex-row  bg-[#1E40AF] text-white navbar shadow-lg shadow-black/10 h-16">
+                <div className="flex items-center p-5">
+                    <img src={logo} alt="logo" className="w-48"/>
+                </div>
+                <div className="p-5">
+                    <ul className="flex flex-row space-x-5 items-center">
+                        <li className="nav-item">Products</li>
+                        <li className="nav-item">Orders</li>
+                        <li className="nav-item">Customers</li>
+                    </ul>
+                </div>
+            </nav>
         </>
     );
 }
